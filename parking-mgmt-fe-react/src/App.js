229a689729e6968277c3073b4c5a0f412aaf246f
Geektimus <alexander.kno@gmail.com>
2018-01-30 19:43:55 -0500

    Remove hardcoded data and use the users directly from the backend

--- parking-mgmt-fe-react/src/App.js
+++ parking-mgmt-fe-react/src/App.js
@@ -3,11 +3,6 @@ import "./App.css";
 
 import Users from "./components/Users";
 
-const users = [
-  { id: "1", firstname: "John", lastname: "Doe" },
-  { id: "2", firstname: "Jane", lastname: "Doe" }
-];
-
 class App extends Component {
   render() {
     return (
@@ -15,7 +10,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Parking Management</h1>
         </header>
-        <Users userList={users} />
+        <Users />
       </div>
     );
   }
--- parking-mgmt-fe-react/src/components/Users.jsx
+++ parking-mgmt-fe-react/src/components/Users.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 
 class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users: [] };
+  }
+
   render() {
-    let userLIs = this.props.userList.map(user => (
-      <li id={user.id} key={user.id}>
+    console.log("Component Rendered");
+    let userLIs = this.state.users.map(user => (
+      <li id={user.ID} key={user.ID}>
         {user.firstname} {user.lastname}
       </li>
     ));
     return <ul>{userLIs}</ul>;
+  }
+
+  componentDidMount() {
+    fetch(`http://parking-mgmt-react:9000/api/users`)
+      .then(result => result.json())
+      .then(users => this.setState({ users }));
   }
 }
 
